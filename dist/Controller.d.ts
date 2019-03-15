41209e04e88326244f27ab7b81a4f0ddff7ea537
James Milner <jamesmilner22@hotmail.com>
2019-03-15 22:51:55 +0000

    Prettier fix for dist

--- dist/Controller.d.ts
+++ dist/Controller.d.ts
@@ -4,24 +4,24 @@ import { Source } from "./THREEAR";
 import { PatternMarker } from "./PatternMarker";
 import { BarcodeMarker } from "./BarcodeMarker";
 export interface MarkerPositioningParameters {
-    smooth: boolean;
-    smoothCount: 5;
-    smoothTolerance: 0.01;
-    smoothThreshold: 2;
+	smooth: boolean;
+	smoothCount: 5;
+	smoothTolerance: 0.01;
+	smoothThreshold: 2;
 }
 export interface ControllerParameters {
-    source: Source;
-    positioning: MarkerPositioningParameters;
-    debug: boolean;
-    changeMatrixMode: "modelViewMatrix" | "cameraTransformMatrix";
-    detectionMode: "color" | "color_and_matrix" | "mono" | "mono_and_matrix";
-    matrixCodeType: string;
-    cameraParametersUrl: string | Uint8Array;
-    maxDetectionRate: number;
-    canvasWidth: number;
-    canvasHeight: number;
-    patternRatio: number;
-    imageSmoothingEnabled: boolean;
+	source: Source;
+	positioning: MarkerPositioningParameters;
+	debug: boolean;
+	changeMatrixMode: "modelViewMatrix" | "cameraTransformMatrix";
+	detectionMode: "color" | "color_and_matrix" | "mono" | "mono_and_matrix";
+	matrixCodeType: string;
+	cameraParametersUrl: string | Uint8Array;
+	maxDetectionRate: number;
+	canvasWidth: number;
+	canvasHeight: number;
+	patternRatio: number;
+	imageSmoothingEnabled: boolean;
 }
 /**
  * The controller is returned from THREE ARs initialize method, in the returned promise.
@@ -30,28 +30,28 @@ export interface ControllerParameters {
  * @param parameters parameters for determining things like detection mode and smoothing
  */
 export declare class Controller extends THREE.EventDispatcher {
-    postInit: Promise<any>;
-    private parameters;
-    private arController;
-    private smoothMatrices;
-    private _updatedAt;
-    private _artoolkitProjectionAxisTransformMatrix;
-    private _markers;
-    constructor(parameters: ControllerParameters);
-    setParameters(parameters: any): void;
-    onResize(renderer: WebGLRenderer): void;
-    update(srcElement: any): boolean;
-    trackMarker(marker: PatternMarker | BarcodeMarker): void;
-    private initialize;
-    private _initArtoolkit;
-    private getProjectionMatrix;
-    private trackPatternMarker;
-    private trackBarcode;
-    private onMarkerFound;
-    /**
-     * When you actually got a new modelViewMatrix, you need to perfom a whole bunch
-     * of things. it is done here.
-     */
-    private updateWithModelViewMatrix;
+	postInit: Promise<any>;
+	private parameters;
+	private arController;
+	private smoothMatrices;
+	private _updatedAt;
+	private _artoolkitProjectionAxisTransformMatrix;
+	private _markers;
+	constructor(parameters: ControllerParameters);
+	setParameters(parameters: any): void;
+	onResize(renderer: WebGLRenderer): void;
+	update(srcElement: any): boolean;
+	trackMarker(marker: PatternMarker | BarcodeMarker): void;
+	private initialize;
+	private _initArtoolkit;
+	private getProjectionMatrix;
+	private trackPatternMarker;
+	private trackBarcode;
+	private onMarkerFound;
+	/**
+	 * When you actually got a new modelViewMatrix, you need to perfom a whole bunch
+	 * of things. it is done here.
+	 */
+	private updateWithModelViewMatrix;
 }
 export default Controller;
--- dist/Source.d.ts
+++ dist/Source.d.ts
@@ -1,14 +1,14 @@
 interface SourceParameters {
-    camera: THREE.Camera | null;
-    renderer: THREE.WebGLRenderer | null;
-    sourceType: "webcam" | "image" | "video";
-    sourceUrl: string;
-    facingMode: "user" | "environment";
-    deviceId: any;
-    sourceWidth: number;
-    sourceHeight: number;
-    displayWidth: number;
-    displayHeight: number;
+	camera: THREE.Camera | null;
+	renderer: THREE.WebGLRenderer | null;
+	sourceType: "webcam" | "image" | "video";
+	sourceUrl: string;
+	facingMode: "user" | "environment";
+	deviceId: any;
+	sourceWidth: number;
+	sourceHeight: number;
+	displayWidth: number;
+	displayHeight: number;
 }
 /**
  * Source describes how and where THREE AR should accept imagery to
@@ -16,24 +16,24 @@ interface SourceParameters {
  * @param parameters parameters for determining if it should come from a webcam or a video
  */
 export declare class Source {
-    private domElement;
-    private parameters;
-    private currentTorchStatus;
-    constructor(parameters: SourceParameters);
-    setParameters(parameters: any): void;
-    readonly renderer: import("three").WebGLRenderer | null;
-    readonly camera: import("three").Camera | null;
-    initialize(): Promise<{}>;
-    hasMobileTorch(domElement: any): boolean;
-    /**
-     * toggle the flash/torch of the mobile fun if applicable.
-     * Great post about it https://www.oberhofer.co/mediastreamtrack-and-its-capabilities/
-     */
-    toggleMobileTorch(domElement: any): void;
-    onResizeElement(): void;
-    copyElementSizeTo(otherElement: any): void;
-    private _initSourceImage;
-    private _initSourceVideo;
-    private _initSourceWebcam;
+	private domElement;
+	private parameters;
+	private currentTorchStatus;
+	constructor(parameters: SourceParameters);
+	setParameters(parameters: any): void;
+	readonly renderer: import("three").WebGLRenderer | null;
+	readonly camera: import("three").Camera | null;
+	initialize(): Promise<{}>;
+	hasMobileTorch(domElement: any): boolean;
+	/**
+	 * toggle the flash/torch of the mobile fun if applicable.
+	 * Great post about it https://www.oberhofer.co/mediastreamtrack-and-its-capabilities/
+	 */
+	toggleMobileTorch(domElement: any): void;
+	onResizeElement(): void;
+	copyElementSizeTo(otherElement: any): void;
+	private _initSourceImage;
+	private _initSourceVideo;
+	private _initSourceWebcam;
 }
 export default Source;
