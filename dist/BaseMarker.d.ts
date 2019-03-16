517cbf95231bcd4473d0568eb29c03b9c5854b80
James Milner <jamesmilner22@hotmail.com>
2019-03-16 21:27:34 +0000

    Fix minConfidence issue for PatternMarker, create BaseMarker abstract class

---
+++ dist/BaseMarker.d.ts
@@ -0,0 +1,6 @@
+export abstract class BaseMarker {
+	id: number;
+	size: number;
+	minConfidence: number;
+	constructor(id: number, parameters: any);
+}
--- src/PatternMarker.ts
+++ src/PatternMarker.ts
@@ -1,19 +1,19 @@
 import { Object3D } from "three";
+import { BaseMarker } from "./BaseMarker";
 
 /**
  * PatternMarker takes a given image (as a URL) and use this as the pattern to detect to place
  * a given marker from three.js, in this case an given Object3D
  * @param parameters parameters for determining things the pattern URL and minimum confidence
  */
-export class PatternMarker {
-	public static markerCount = 0;
-	public id: number;
-	public size: number;
-	public minConfidence: number;
+export class PatternMarker extends BaseMarker {
+	public static count = 0;
 	public patternUrl: string;
 	public markerObject: Object3D;
 
 	constructor(parameters: any) {
+		super(PatternMarker.count++, parameters);
+
 		if (!parameters.patternUrl) {
 			throw Error("Pattern Marker requires a patternUrl to be passed");
 		}
@@ -22,9 +22,6 @@ export class PatternMarker {
 			throw Error("Pattern Marker requires a markerObject to be passed");
 		}
 
-		this.id = PatternMarker.markerCount++;
-		this.size = parameters.size || 1;
-		this.minConfidence = parameters.size || 0.6;
 		this.patternUrl = parameters.patternUrl;
 		this.markerObject = parameters.markerObject;
 
