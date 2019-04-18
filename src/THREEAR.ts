cab7dffbb745d5886029bbd4a251ffbaac4f66be
0b01001101 <martin.loretz@hotmail.com>
2019-04-18 15:22:04 +0100

    Fixed some ts bugs, made params optimal

--- src/THREEAR.ts
+++ src/THREEAR.ts
@@ -4,11 +4,12 @@ import BarcodeMarker from "./BarcodeMarker";
 import { Controller, ControllerParameters } from "./Controller";
 
 let controller: Controller;
-function initialize(parameters: ControllerParameters) {
+function initialize(parameters: Partial<ControllerParameters>) {
 	if (controller === undefined || controller.disposed === true) {
 		controller = new Controller(parameters);
 	}
 	return controller.postInit;
 }
 
+export type Controller = typeof controller;
 export { Source, initialize, PatternMarker, BarcodeMarker };
